3235a636bb8be1de360f2261ae56919a3f04e648
Sumair Qaisar <sumairqaisar992@gmail.com>
2022-12-19 00:19:04 +0500

    Setup react typescript application with webpack and babel

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+// App.tsx
+import React from "react";
+import { Test } from "./Test";
+export const App: React.FC = () => {
+	return <Test loading={false} />;
+};
---
+++ src/Test.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Test: React.FC<{ loading: boolean }> = ({ loading }) => {
+	return <div>Test</div>;
+};
